02aa396a2d8ef5597c44c393af54ca4383ddf527
nikitaberdnikovs <143938205+nikitaberdnikovs@users.noreply.github.com>
2024-01-18 10:35:43 +0200

    Update wiki-projects.py

--- wiki-projects.py
+++ wiki-projects.py
@@ -15,12 +15,10 @@ url = str(ws['A' + '2'].value)
 driver.get(url)
 time.sleep(2)
 name = driver.find_element(By.CSS_SELECTOR, 'span.mw-page-title-main').text
-# print(driver.page_source)
 elements = driver.find_elements(By.XPATH, '//p | //ul')
 i = 4
 j = 4
 for element in elements:
-    # Find all "a" elements within the "p" element
     a_elements = element.find_elements(By.TAG_NAME, 'a')
     for lnk in a_elements:
          all_links = str(lnk.get_attribute('href'))
